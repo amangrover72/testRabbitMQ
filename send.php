66cab6c762ff3cae70f1a80c3733821eb81788fc
Aman <amanfromoakland@gmail.com>
2022-03-19 00:51:43 -0400

    initial commit

---
+++ send.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$connection = new AMQPStreamConnection('10.4.90.102', 5672, 'log', 'stonx_log');
+$channel = $connection->channel();
+
+$channel->queue_declare('hello', false, false, false, false);
+
+$msg = new AMQPMessage('Hello World!');
+$channel->basic_publish($msg, '', 'hello');
+
+echo " [x] Sent 'Hello World!'\n";
+
+$channel->close();
+$connection->close();
+?>
